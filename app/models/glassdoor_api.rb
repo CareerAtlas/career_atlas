e11bbdad3c53e8cd2a2aef41b65348b8bd25d946
Robby Dore <Rdore88@gmail.com>
2017-04-18 13:32:43 -0400

    Fixed glassdoor api model.

--- app/models/glassdoor_api.rb
+++ app/models/glassdoor_api.rb
@@ -3,16 +3,17 @@ class GlassdoorApi
   def self.search_company(ipaddress, browser, company)
     HTTParty.get("http://api.glassdoor.com/api/api.htm?",
       {
-        params: [
-          {v: 1},
-          {format: "json"},
-          {"t.p": 142249},
-          {"t.k": "kHfjVZZplCE"},
-          {userip: ipaddress},
-          {useragent: browser},
-          {action: "employers"},
-          {q: company}
-        ]
+        query:
+        {
+          v: 1,
+          format: "json",
+          "t.p": 142249,
+          "t.k": "kHfjVZZplCE",
+          userip: ipaddress,
+          useragent: browser,
+          action: "employers",
+          q: company
+        }
       }
     )
   end
