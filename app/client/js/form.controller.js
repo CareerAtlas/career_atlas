f5c7141b6d46cf29c462b337b5d57427c371d749
Jennifer Oakes <joakes10@gmail.com>
2017-04-18 10:10:37 -0400

    .

---
+++ app/client/js/form.controller.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular.module('career_atlas')
+    .controller('FormController', FormController);
+
+    console.log('inside the form controller here');
+
+}());
