73b0d1185c85f102acc08b640edda19bb1bc10c3
Robby Dore <Rdore88@gmail.com>
2017-04-19 10:27:56 -0400

    Refactored controller to make smaller methods.

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -2,7 +2,14 @@ class Api::JobsController < ApplicationController
 
   def index
     jobs = IndeedApi.search_jobs(params)
-    job_info = jobs["results"].map do |job|
+    job_info = info_for_output(jobs["results"])
+    render json:job_info
+  end
+
+  private
+
+  def info_for_output(jobs)
+    jobs.map do |job|
       {
         jobtitle: job["jobtitle"],
         company: job["company"],
@@ -13,6 +20,5 @@ class Api::JobsController < ApplicationController
         date_posted: job["formattedRelativeTime"]
       }
     end
-    render json:job_info
   end
 end
