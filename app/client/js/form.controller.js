c8405d9f5369ea7451812566b9e1fd8c5468d5e3
Jennifer Oakes <joakes10@gmail.com>
2017-04-19 15:54:46 -0400

    Created an empty array of jobs on the controller

--- app/client/js/form.controller.js
+++ app/client/js/form.controller.js
@@ -10,6 +10,8 @@
 
     function FormController(FormService) {
       let vm = this;
+      vm.jobs = [];
+
       vm.search = {
         job_type: 'Full-Time',
         radius: '20',
@@ -18,8 +20,10 @@
       vm.submit = function submit(search) {
         console.log(search);
 
-        FormService.createJobSearch(search);
-        console.log(search);
+        FormService.createJobSearch(search)
+          .then(function handleData(data) {
+            vm.jobs = data;
+          });
       };
     }
 }());
