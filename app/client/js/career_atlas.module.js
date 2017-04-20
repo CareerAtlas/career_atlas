3513cbcc016ad4747a7fb1f2fa75c6e90de5ae16
Christine Ash <ChristineTheBrave@Christines-MBP.lan>
2017-04-20 13:38:27 -0400

    Added ui-view to main in index. Added states and routers to module file. Inserted home page items into template

--- app/client/js/career_atlas.module.js
+++ app/client/js/career_atlas.module.js
@@ -6,14 +6,25 @@
 
   routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
   function routerConfig($stateProvider, $urlRouterProvider) {
-
+    console.log('this is routerconfig fn');
     $urlRouterProvider.when('','/');
 
     $urlRouterProvider.otherwise('/not-found');
 
     $stateProvider
       .state({
-
+        name: 'home',
+        url: '/',
+        templateUrl: 'templates/home.template.html',
+        controller: 'JobController',
+        controllerAs: 'jobCtrl'
+      })
+      .state({
+        name: 'user-login',
+        url: '/login',
+        templateUrl: 'templates/user-login.template.html',
+        controller: 'UserController',
+        controllerAs: 'userCtrl'
       });
 
 
