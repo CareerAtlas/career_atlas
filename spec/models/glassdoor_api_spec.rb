5ea7f254eee594b68163273903020d882e3b52cc
Robby Dore <Rdore88@gmail.com>
2017-04-20 15:58:36 -0400

    Created and passed first test for walkscore api, refactored glassdoor test.

--- spec/models/glassdoor_api_spec.rb
+++ spec/models/glassdoor_api_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GlassdoorApi, type: :model do
     stub_request(:get, /api.glassdoor.com/)
       .to_return(
         body: File.read(Rails.root.join("spec", "stubbed_requests", "company_search.json")),
-        headers: {'Content-Type' => 'application/json'}
+        headers: {"Content-Type" => "application/json"}
       )
       answer = GlassdoorApi.search_company(params)
       expect(answer["response"]["employers"][0]["website"]).to eq "www.verisign.com"
--- spec/models/walkscore_api_spec.rb
+++ spec/models/walkscore_api_spec.rb
@@ -3,6 +3,22 @@ require "rails_helper"
 RSpec.describe WalkscoreApi, type: :model do
 
   it "receives response when asking walk score for scores" do
-    
+    params = {
+      wsapikey: "5108b6df6ffbdbd8ad43d4a5dc25ee8a",
+      lat: 40.751652,
+      lon: -73.975311,
+      address: "405 Lexington Ave, New York City 10174",
+      transit: 1,
+      bike: 1,
+      format: "json"
+    }
+    stub_request(:get, /api.walkscore.com/)
+      .to_return(
+        body: File.read(Rails.root.join("spec", "stubbed_requests", "walkscore_search.json")),
+        headers: {"Content-Type" => "application/json"}
+      )
+      answer = WalkscoreApi.search_walkscore(params)
+      expect(answer["walkscore"]).to eq 100
+
   end
 end
