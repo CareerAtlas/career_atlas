4ee10528c0a4848add75d867640a7cbbb7ff9f70
Christine Ash <ChristineTheBrave@Christines-MBP.lan>
2017-04-20 10:54:01 -0400

    Added html template for login. Added initial code to module for ui routing.

--- app/client/js/career_atlas.module.js
+++ app/client/js/career_atlas.module.js
@@ -1,7 +1,23 @@
 (function() {
   'use strict';
 
-  angular.module( 'career_atlas', [] );
+  angular.module( 'career_atlas', ['ui.router'] )
+    .config(routerConfig);
+
+  routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function routerConfig($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.when('','/');
+
+    $urlRouterProvider.otherwise('/not-found');
+
+    $stateProvider
+      .state({
+
+      });
+
+
+  }
 
 
 }());
