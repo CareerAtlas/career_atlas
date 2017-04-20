ea9c127c50bca7881e644a3da77905b8756b6761
Robby Dore <Rdore88@gmail.com>
2017-04-20 14:16:20 -0400

    Created test files for the walkscore api.

--- spec/models/glassdoor_api_spec.rb
+++ spec/models/glassdoor_api_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe GlassdoorApi, type: :model do
-  params = { company: "Verisign" }
+
   it "receives response when asking Glassdoor about a company" do
+    params = { company: "Verisign" }
     stub_request(:get, /api.glassdoor.com/)
       .to_return(
         body: File.read(Rails.root.join("spec", "stubbed_requests", "company_search.json")),
---
+++ spec/models/walkscore_api_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe WalkscoreApi, type: :model do
+
+  it "receives response when asking walk score for scores" do
+    
+  end
+end
