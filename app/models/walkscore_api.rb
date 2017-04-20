c24fae128afe8fe5feb8235f06682aa9cdf822f5
Robby Dore <Rdore88@gmail.com>
2017-04-20 11:53:45 -0400

    Created initial method to connect with walk score api.

--- app/models/walkscore_api.rb
+++ app/models/walkscore_api.rb
@@ -2,7 +2,22 @@ class WalkscoreApi
   include HTTParty
 
   def self.search_walkscore(search_params)
-    
+    HTTParty.get("http://api.walkscore.com/score?",
+      {
+        query:
+        {
+          lat: search_params["latitude"],
+          lon: search_params["longitude"],
+          address: search_params["address"],
+          wsapikey: "5108b6df6ffbdbd8ad43d4a5dc25ee8a",
+          transit: 1,
+          bike: 1,
+          format: "JSON"
+        }
+
+      }
+
+    )
   end
 
 
