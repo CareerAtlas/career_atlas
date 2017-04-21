5e634dc6bd4764d207c7228cd7985e6596a342d3
Robby Dore <Rdore88@gmail.com>
2017-04-21 12:13:30 -0400

    Created new migration to add authorization_token and changed the schema.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :jobs, only: [:index, :show, :create]
     resources :users, only: [:index, :create]
     resources :companies, only: [:index, :show, :create]
+    resources :users, only: [:show, :create]
 
   end
 end
