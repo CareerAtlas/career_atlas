db56e104aacdd9cb651170b87de74ec58053624c
Robby Dore <Rdore88@gmail.com>
2017-04-23 14:19:36 -0400

    Passed initial authorization test.

--- spec/controllers/api/authorizations_controller_spec.rb
+++ spec/controllers/api/authorizations_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::AuthorizationsController, type: :controller do
 
+  it "allows someone to log in" do
+    moose = User.create!(name: "Moose", email: "Moose@gooddogs.com", password: "treats", password_confirmation: "treats")
+    post :create, params: { email: moose.email, password: "treats" }
+    assert response.ok?
+  end
+
 end
