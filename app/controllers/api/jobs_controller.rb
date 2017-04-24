e28ee4dd81b633ab5b85530d343a71e2e4b79d12
Robby Dore <Rdore88@gmail.com>
2017-04-24 13:28:00 -0400

    Initial setup to save all jobs.

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -3,11 +3,11 @@ class Api::JobsController < ApplicationController
   def index
     jobs = IndeedApi.search_jobs(params)
     job_info = info_for_output(jobs["results"])
+    save_jobs(job_info)
     render json: job_info
   end
 
   private
-
   def info_for_output(jobs)
     jobs.map do |job|
       {
@@ -20,6 +20,14 @@ class Api::JobsController < ApplicationController
         location: job["formattedLocationFull"],
         date_posted: job["formattedRelativeTime"]
       }
+    end
+  end
+
+  def save_jobs(jobs)
+    jobs.each do |job|
+      unless Job.find_by(job.key)
+        Job.create(job)
+      end
     end
   end
 end
