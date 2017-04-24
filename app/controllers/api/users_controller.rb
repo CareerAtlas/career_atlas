4e51a66cc3af4868d6224bbbe533f906521a6f6b
Robby Dore <Rdore88@gmail.com>
2017-04-24 12:00:06 -0400

    Fixed users controller and refactored a user test.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
   def create
     @new_user = User.new(user_params)
     @new_user.secure_random
-    if @new_user.save!
+    if @new_user.save
       render json: {authorization: @new_user.authorization_token}
     else
       render json: { message: error.to_s, status: :unprocessable_entity }
