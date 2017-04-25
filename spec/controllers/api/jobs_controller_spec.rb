9e2a75f0ae0649d08a9f8e43f23880ace692ae3b
Robby Dore <Rdore88@gmail.com>
2017-04-25 12:00:52 -0400

    Attempted to write a new test.

--- spec/controllers/api/jobs_controller_spec.rb
+++ spec/controllers/api/jobs_controller_spec.rb
@@ -13,4 +13,10 @@ RSpec.describe Api::JobsController, type: :controller do
     get :index, params: search_params
     assert response.ok?
   end
+
+  it "saves a job" do
+    params = {job_title: "Software Developer", job_key: "66d032cd079cc5ab", latitude: "38.956764", longitude: "-77.361435", location: "Reston, VA", company: "VeriSign"}
+    post :create, params: params, json_payload: User.create!({ name: "Moose", email: "moose@gmail.com", password: "treat", password_confirmation: "treat" })
+    assert response.ok?
+  end
 end
