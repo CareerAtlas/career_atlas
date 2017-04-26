4acc338db29b089f7831edc4375db90a5e6babb0
Jennifer Oakes <joakes10@gmail.com>
2017-04-26 15:56:51 -0400

    Updates on job service spec

--- spec/client/job.service.spec.js
+++ spec/client/job.service.spec.js
@@ -4,13 +4,18 @@
  let expect = chai.expect;
 
  describe('job service', function(){
-
    let JobService;
+   let $httpBackend;
 
-   beforeEach(module('career_atlas'));
+   beforeEach(module('career_atlas')); //telling angular-mocks to create an ng-app
 
-   beforeEach(inject(function(_JobService_){
+   beforeEach(inject(function(_$httpBackend_, _JobService_){
      JobService = _JobService_;
+     $httpBackend = _$httpBackend_;
+
+     $httpBackend
+      .whenGET('/api/jobs/')
+      .respond([{}, {}]);
    }));
 
    it('should expect the function to be function', function() {
