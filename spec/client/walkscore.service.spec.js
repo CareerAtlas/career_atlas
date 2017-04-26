0984f569b3badbfc775f6b54ced283ec85ae81f3
Jennifer Oakes <joakes10@gmail.com>
2017-04-26 12:05:51 -0400

    Added initial test file and first test for walkscore service

---
+++ spec/client/walkscore.service.spec.js
@@ -0,0 +1,20 @@
+(function() {
+ 'use strict';
+
+ let expect = chai.expect;
+
+ describe('walkscore service', function(){
+
+   let WalkscoreService;
+
+   beforeEach(module('career_atlas'));
+
+   beforeEach(inject(function(_WalkscoreService_){
+     WalkscoreService = _WalkscoreService_;
+   }));
+
+   it('should expect the function to be function', function() {
+     expect(WalkscoreService.getWalkscoreInformation).to.be.a('function');
+   });
+ });
+}());
