36046b9c183a3e06f7162e34bb012faf08148173
Jennifer Oakes <joakes10@gmail.com>
2017-04-26 10:21:15 -0400

    Added front end test folder

---
+++ spec/client/job.service.spec.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  let expect = chai.expect;
+
+  describe('job service', function(){
+
+    let JobService;
+
+    beforeEach(module('career_atlas'));
+
+    beforeEach(inject(function(_JobService_){
+      JobService = _JobService_;
+    }));
+
+    it('should expect the function to be function', function() {
+      expect(JobService.createJobSearch).to.be.a('function');
+    });
+
+  });
+
+}());
