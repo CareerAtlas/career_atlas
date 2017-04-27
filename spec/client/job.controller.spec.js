c0748909ce7ab9aaf85be801bf24e5424fc12779
Jennifer Oakes <joakes10@gmail.com>
2017-04-27 17:46:10 -0400

    Reorganization for success and fail case testing

--- spec/client/job.controller.spec.js
+++ spec/client/job.controller.spec.js
@@ -36,44 +36,6 @@
         ]);
       };
       mockJobService.createJobSearch.numTimesCalled = 0;
-    }));
-
-
-    beforeEach(inject(function($controller, $rootScope, _$q_) {
-      $q = _$q_;
-      mockCompanyService.getGlassdoorCompanyInformation = function getGlassdoorCompanyInformation() {
-        mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled++;
-        console.info(mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled);
-        return $q.resolve(
-          {
-            "company": "Metro Systems (VA)",
-            "logo": "https://media.glassdoor.com/sqll/643465/metro-systems-va-squarelogo-1425026414371.png",
-            "overall_rating": "3.8",
-            "culture_rating": "3.8",
-            "leadership_rating": "3.8",
-            "compensation_rating": "2.0",
-            "opportunity_rating": "3.5",
-            "recommend_to_friend_rating": "3.2",
-            "work_life_balance_rating": "3.1"
-          }
-        );
-      };
-      mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled = 0;
-    }));
-
-
-    beforeEach(inject(function($controller, $rootScope, _$q_) {
-      $q = _$q_;
-      mockWalkscoreService.getWalkscoreInformation = function getWalkscoreInformation() {
-        mockWalkscoreService.getWalkscoreInformation.numTimesCalled++;
-        return $q.resolve(
-          {
-            "walk_score": "67",
-            "description": "Somewhat Walkable"
-          }
-        );
-      };
-      mockWalkscoreService.getWalkscoreInformation.numTimesCalled = 0;
       let scope = $rootScope.$new();
       JobController = $controller('JobController', { $scope:scope });
     }));
@@ -98,19 +60,63 @@
       expect(JobController.jobs).to.be.an('array');
     });
 
-    it('should display job information in job object', function() {
-      let marker = {
-        data: {
-          company: "Metro Star",
-          latitude: 38.862637,
-          longitude: -77.19231,
-          location: "Annandale, VA"
-        }
-      };
-      console.info(marker);
-      JobController.showJobInformation(marker);
-      expect(JobController.displayedJob).to.be.an('object');
-      expect(JobController.displayedJob.glassdoorData).to.be.an('object');
+    describe('this should test showJobInformation', function() {
+      describe('successful', function() {
+        beforeEach(inject(function($controller, $rootScope, _$q_) {
+          $q = _$q_;
+          mockCompanyService.getGlassdoorCompanyInformation = function getGlassdoorCompanyInformation() {
+            mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled++;
+            console.info(mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled);
+            return $q.resolve(
+              {
+                "company": "Metro Systems (VA)",
+                "logo": "https://media.glassdoor.com/sqll/643465/metro-systems-va-squarelogo-1425026414371.png",
+                "overall_rating": "3.8",
+                "culture_rating": "3.8",
+                "leadership_rating": "3.8",
+                "compensation_rating": "2.0",
+                "opportunity_rating": "3.5",
+                "recommend_to_friend_rating": "3.2",
+                "work_life_balance_rating": "3.1"
+              }
+            );
+          };
+          mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled = 0;
+        }));
+
+        beforeEach(inject(function($controller, $rootScope, _$q_) {
+          $q = _$q_;
+          mockWalkscoreService.getWalkscoreInformation = function getWalkscoreInformation() {
+            mockWalkscoreService.getWalkscoreInformation.numTimesCalled++;
+            return $q.resolve(
+              {
+                "walk_score": "67",
+                "description": "Somewhat Walkable"
+              }
+            );
+          };
+          mockWalkscoreService.getWalkscoreInformation.numTimesCalled = 0;
+          let scope = $rootScope.$new();
+          JobController = $controller('JobController', { $scope:scope });
+        }));
+
+        it('should display job information in job object', function() {
+          let marker = {
+            data: {
+              company: "Metro Star",
+              latitude: 38.862637,
+              longitude: -77.19231,
+              location: "Annandale, VA"
+            }
+          };
+          console.info(marker);
+          JobController.showJobInformation(marker);
+          expect(JobController.displayedJob).to.be.an('object');
+          expect(JobController.displayedJob.glassdoorData).to.be.an('object');
+        });
+
+
+      });
     });
 
   });
