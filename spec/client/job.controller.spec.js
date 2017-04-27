4d307c796b9507201ca54d5cfa3577c1d0638109
Jennifer Oakes <joakes10@gmail.com>
2017-04-27 10:49:10 -0400

    Added another test

--- spec/client/job.controller.spec.js
+++ spec/client/job.controller.spec.js
@@ -24,25 +24,70 @@
         mockJobService.createJobSearch.numTimesCalled++;
         return Promise.resolve( [
           {
-            job_title: "search.job_title",
-            job_type: "search.job_type",
-            radius: "search.radius",
-            location: "search.location"
+            "jobtitle": "Ruby on Rails Developer",
+            "company": "Metro Systems, Inc.",
+            "url": "http://www.indeed.com/viewjob?jk=6f111ea0068ecd98&…Y&indpubnum=8417063092021675&atk=1benqfrsuausg9d4",
+            "latitude": 38.862637,
+            "longitude": -77.19231,
+            "date_posted": "26 days ago"
           }
         ]);
       };
-
       mockJobService.createJobSearch.numTimesCalled = 0;
-      // mock other services...
-
       let scope = $rootScope.$new();
       JobController = $controller('JobController', { $scope:scope });
+    }));
 
+    beforeEach(inject(function($controller, $rootScope) {
+      mockCompanyService.getGlassdoorCompanyInformation = function getGlassdoorCompanyInformation() {
+        mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled++;
+        return Promise.resolve( [
+          {
+            "company": "Metro Systems (VA)",
+            "logo": "https://media.glassdoor.com/sqll/643465/metro-systems-va-squarelogo-1425026414371.png",
+            "overall_rating": "3.8",
+            "culture_rating": "3.8",
+            "leadership_rating": "3.8",
+            "compensation_rating": "2.0",
+            "opportunity_rating": "3.5",
+            "recommend_to_friend_rating": "3.2",
+            "work_life_balance_rating": "3.1"
+          }
+        ]);
+      };
+      mockCompanyService.getGlassdoorCompanyInformation.numTimesCalled = 0;
+      let scope = $rootScope.$new();
+      JobController = $controller('JobController', { $scope:scope });
+    }));
+
+    beforeEach(inject(function($controller, $rootScope) {
+      mockWalkscoreService.getWalkscoreInformation = function getWalkscoreInformation() {
+        mockWalkscoreService.getWalkscoreInformation.numTimesCalled++;
+        return Promise.resolve(
+          {
+            "walk_score": "67",
+            "description": "Somewhat Walkable"
+          }
+        );
+      };
+      mockWalkscoreService.getWalkscoreInformation.numTimesCalled = 0;
+      let scope = $rootScope.$new();
+      JobController = $controller('JobController', { $scope:scope });
     }));
 
     it('should be a function', function() {
       expect(JobController.showJobInformation).to.be.a('function');
       expect(JobController.submit).to.be.a('function');
     });
+
+    it('should return an object', function() {
+      let result = JobController.showJobInformation();
+      expect(result).to.equal(undefined);
+    });
+
+
+
+
+
   });
 }());
