86c9d4460cb553776267f735d314387afbcbc04a
Jennifer Oakes <joakes10@gmail.com>
2017-05-01 13:38:04 -0400

    Added show list of saved jobs functionality

--- app/client/js/career_atlas.module.js
+++ app/client/js/career_atlas.module.js
@@ -38,7 +38,7 @@
         name: 'saved-jobs',
         url: '/saved-jobs',
         templateUrl: 'templates/saved-jobs.template.html',
-        controller: 'SavedJobsController',
+        controller: 'JobController',
         controllerAs: 'savedJobsCtrl'
       })
       .state({
