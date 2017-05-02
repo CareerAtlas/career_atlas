ad8b54ebccc4a8ed56cf46413337611c38125d58
Robby Dore <Rdore88@gmail.com>
2017-05-02 16:45:28 -0400

    Created tests for the functionality to see if a user has saved a job from the IndeedApi call.

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -2,6 +2,6 @@ class Api::JobsController < ApplicationController
 
   def index
     jobs = IndeedApi.search_jobs(params)
-    render json: jobs
+    render json: SavedJob.check_for_current_user(jobs, current_user)
   end
 end
--- app/models/saved_job.rb
+++ app/models/saved_job.rb
@@ -9,4 +9,30 @@ class SavedJob < ApplicationRecord
     job = Job.create_or_update_job_by_key(job_key)
     SavedJob.new(job: job, user: user)
   end
+
+  def self.check_for_current_user(jobs, user)
+    if user
+      has_current_user_saved_these_jobs(jobs, user)
+    else
+      no_saved_jobs(jobs)
+    end
+  end
+
+  def self.has_current_user_saved_these_jobs(jobs, user)
+    jobs.each do |job|
+      if user.saved_jobs.joins(:job).find_by("jobs.job_key": job[:job_key])
+        job[:saved] = true
+      else
+        job[:saved] = false
+      end
+    end
+
+  end
+
+  def self.no_saved_jobs(jobs)
+    jobs.each do |job|
+      job[:saved] = false
+    end
+  end
+
 end
